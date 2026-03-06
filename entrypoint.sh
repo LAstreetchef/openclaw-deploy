69be5221c14d6ca9a47d8aa84d0e04f29233896c
LAstreetchef <message4u@secretmessage4u.com>
2026-03-06 03:42:48 -0600

    remove invalid controlUi key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "START"
 mkdir -p /root/.clawdbot
-printf '{"gateway":{"port":10000,"mode":"local","bind":"lan","auth":{"mode":"token","token":"88e8de06118f2f1cabb8442b04d0b39ee6dc24bf3c301ac0"},"controlUi":{"dangerouslyAllowHostHeaderOriginFallback":true}}}' > /root/.clawdbot/clawdbot.json
+printf '{"gateway":{"port":10000,"mode":"local","bind":"lan","auth":{"mode":"token","token":"88e8de06118f2f1cabb8442b04d0b39ee6dc24bf3c301ac0"}}}' > /root/.clawdbot/clawdbot.json
 echo "CONFIG WRITTEN"
 exec clawdbot gateway --port 10000 --allow-unconfigured --force
